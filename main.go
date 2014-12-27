387ad55b3e255c19efbc8284878288cde9335f09
Dmitri Shuralyov <shurcooL@gmail.com>
2014-12-26 16:16:44 -0800

    Add README with package vision.

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
-// Package goglfw provides a unified API for creating a GL context
-// with backends for desktop (via glfw) and browser (via canvas).
+// Package goglfw experimentally provides a glfw-like API
+// with desktop (via glfw) and browser (via canvas) backends.
+//
+// It is used for creating a GL context and receiving events.
 package goglfw
