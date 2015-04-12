ab67ce281f8d790781a41326d3a3457775a0e2a4
Dmitri Shuralyov <shurcooL@gmail.com>
2015-04-11 21:44:53 -0700

    Handle WebGL context creation in browser.
    Create ContextSwitcher interface.
    Require ContextSwitcher in Init.
    Allow use of arbitrary gl bindings by removing coupling with
    github.com/shurcooL/gogl.

--- goglfw.go
+++ goglfw.go
@@ -3,3 +3,10 @@
 //
 // It is used for creating a GL context and receiving events.
 package goglfw
+
+// ContextSwitcher is a general mechanism for switching between contexts.
+type ContextSwitcher interface {
+	// MakeContextCurrent takes a context and makes it current.
+	// If given context is nil, then the current context is detached.
+	MakeContextCurrent(context interface{})
+}
