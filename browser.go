00db1220229dc3436742e70fae97a9e5a25562cb
20zinnm <meyerzinn@gmail.com>
2018-04-15 10:49:06 -0500

    run gofmt

--- browser.go
+++ browser.go
@@ -762,13 +762,13 @@ const (
 type InputMode int
 
 const (
-	CursorMode             InputMode = iota
+	CursorMode InputMode = iota
 	StickyKeysMode
 	StickyMouseButtonsMode
 )
 
 const (
-	CursorNormal   = iota
+	CursorNormal = iota
 	CursorHidden
 	CursorDisabled
 )
@@ -776,7 +776,7 @@ const (
 type ModifierKey int
 
 const (
-	ModShift   ModifierKey = (1 << iota)
+	ModShift ModifierKey = (1 << iota)
 	ModControl
 	ModAlt
 	ModSuper
